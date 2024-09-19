4964474747f1ead8f3043f7c5fc8424a6caefafb
Tweek-Byte <zaka4396@gmail.com>
2024-09-18 19:19:46 -0700

    basic tasks

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -7,22 +7,4 @@ const displayMessage = (msg) => {
   console.log(msg);
 };
 
-module.exports = displayMessage;/**
- * Displays a message on the STDOUT.
- * @param {String} msg The message to display.
- * @author  Zakaria Aabab
- */
-const displayMessage = (msg) => {
-  console.log(msg);
-};
-
-module.exports = displayMessage;/**
- * Displays a message on the STDOUT.
- * @param {String} msg The message to display.
- * @author  Zakaria Aabab
- */
-const displayMessage = (msg) => {
-  console.log(msg);
-};
-
 module.exports = displayMessage;
